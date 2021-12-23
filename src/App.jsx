f72a0d830fa6adebb811d3d4b117ccf0b195a2ae
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-23 12:27:25 +0100

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+     <h1>Hello world</h1>
+   </div>
+  )
+}
+
+export default App
